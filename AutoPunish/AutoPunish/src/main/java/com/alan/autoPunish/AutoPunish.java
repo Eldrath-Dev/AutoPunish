7f9482ea6656c4c1b225c23360a171ffb15946e0
Atharva <gogosmp2@gmail.com>
2025-09-17 03:18:56 +0530

    Strach

---
+++ AutoPunish/AutoPunish/src/main/java/com/alan/autoPunish/AutoPunish.java
@@ -0,0 +1,17 @@
+package com.alan.autoPunish;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoPunish extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
